cb8f8b53d4917a191fbdce9227eba740f0009ab4
gannh22 <rdeed@kc.rr.com>
2022-01-17 21:09:01 -0600

    drafted flywheel added game/shot.java

---
+++ RapidReact-1806/src/game/shot.java
@@ -0,0 +1,35 @@
+public class shot {
+    Double liftHeight, launcherAngle, baseSpeed, baseSpin;
+
+    public Double getLiftHeight() {
+        return liftHeight;
+    }
+
+    public Double getLauncherAngle() {
+        return launcherAngle;
+    }
+
+    public Double getBaseSpeed() {
+        return baseSpeed;
+    }
+
+    public Double getBaseSpin() {
+        return baseSpin;
+    }
+
+
+    public Boolean getIsPreciseShot() {
+        return isPreciseShot;
+    }
+
+    Boolean isPreciseShot;
+
+    public shot(Double liftHeight, Double launcherAngle, Double baseSpeed, Double baseSpin, Boolean isPreciseShot) {
+        this.liftHeight = liftHeight;
+        this.launcherAngle = launcherAngle;
+        this.baseSpeed = baseSpeed;
+        this.baseSpin = baseSpin;
+        this.isPreciseShot = isPreciseShot;
+    }
+    
+}
