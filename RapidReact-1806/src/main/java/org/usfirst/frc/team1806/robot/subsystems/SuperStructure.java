b8973ce758b75468f9d7a3a87d7c565b1bb764ea
swat1806 <frc1806@gmail.com>
2022-01-27 18:42:40 -0600

    super structure part 2 electric boogaloo

---
+++ RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/subsystems/SuperStructure.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team1806.robot.subsystems;
+
+import org.usfirst.frc.team1806.robot.loop.Looper;
+
+public class SuperStructure implements Subsystem {
+
+    @Override
+    public void writeToLog() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void outputToSmartDashboard() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void zeroSensors() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void registerEnabledLoops(Looper enabledLooper) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDebug(boolean _debug) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void retractAll() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
