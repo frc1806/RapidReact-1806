f2167991e10abe50f408495fb2ac567783d09b18
swat1806 <frc1806@gmail.com>
2022-01-29 15:01:10 -0600

    started conveyor subsystem

---
+++ RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/subsystems/Conveyor.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team1806.robot.subsystems;
+
+import org.usfirst.frc.team1806.robot.loop.Looper;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Conveyor implements Subsystem {
+
+    private TalonSRX TalonSRX;
+
+    public Conveyor(){
+
+    }
+
+    @Override
+    public void writeToLog() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void outputToSmartDashboard() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void zeroSensors() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void registerEnabledLoops(Looper enabledLooper) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDebug(boolean _debug) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void retractAll() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
