2c96ff5eed13a4302e4d684129107e8c0d5a1e5c
swat1806 <frc1806@gmail.com>
2022-02-26 10:49:16 -0600

    added auto path and improved cheezy path to work with 1806

---
+++ RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/auto/paths/SixBallPath3.java
@@ -0,0 +1,37 @@
+﻿package org.usfirst.frc.team1806.robot.auto.paths;
+
+import java.util.ArrayList;
+
+import org.usfirst.frc.team1806.robot.path.Path;
+import org.usfirst.frc.team1806.robot.path.PathBuilder;
+import org.usfirst.frc.team1806.robot.path.PathContainer;
+import org.usfirst.frc.team1806.robot.util.RigidTransform2d;
+import org.usfirst.frc.team1806.robot.util.Rotation2d;
+import org.usfirst.frc.team1806.robot.util.Translation2d;
+
+public class SixBallPath3 implements PathContainer {
+    
+    @Override
+    public Path buildPath() {
+        ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
+        sWaypoints.add(new Waypoint(190,150,0,60));
+        sWaypoints.add(new Waypoint(190,85,10,60));
+        sWaypoints.add(new Waypoint(230,20,10,60));
+        sWaypoints.add(new Waypoint(280,20,0,60));
+
+        return PathBuilder.buildPathFromWaypoints(sWaypoints);
+    }
+    
+    @Override
+    public RigidTransform2d getStartPose() {
+        return new RigidTransform2d(new Translation2d(190, 150), Rotation2d.fromDegrees(180.0)); 
+    }
+
+    @Override
+    public boolean isReversed() {
+        return false; 
+    }
+	// WAYPOINT_DATA: [{"position":{"x":190,"y":150},"speed":60,"radius":0,"comment":""},{"position":{"x":190,"y":85},"speed":60,"radius":10,"comment":""},{"position":{"x":230,"y":20},"speed":60,"radius":10,"comment":""},{"position":{"x":280,"y":20},"speed":60,"radius":0,"comment":""}]
+	// IS_REVERSED: false
+	// FILE_NAME: SixBallPath3
+}
---
+++ RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/auto/paths/SixBallPath4.java
@@ -0,0 +1,37 @@
+﻿package org.usfirst.frc.team1806.robot.auto.paths;
+
+import java.util.ArrayList;
+
+import org.usfirst.frc.team1806.robot.path.Path;
+import org.usfirst.frc.team1806.robot.path.PathBuilder;
+import org.usfirst.frc.team1806.robot.path.PathContainer;
+import org.usfirst.frc.team1806.robot.util.RigidTransform2d;
+import org.usfirst.frc.team1806.robot.util.Rotation2d;
+import org.usfirst.frc.team1806.robot.util.Translation2d;
+
+public class SixBallPath4 implements PathContainer {
+    
+    @Override
+    public Path buildPath() {
+        ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
+        sWaypoints.add(new Waypoint(280,20,0,60));
+        sWaypoints.add(new Waypoint(250,20,20,60));
+        sWaypoints.add(new Waypoint(105,100,40,60));
+        sWaypoints.add(new Waypoint(60,65,0,60));
+
+        return PathBuilder.buildPathFromWaypoints(sWaypoints);
+    }
+    
+    @Override
+    public RigidTransform2d getStartPose() {
+        return new RigidTransform2d(new Translation2d(280, 20), Rotation2d.fromDegrees(180.0)); 
+    }
+
+    @Override
+    public boolean isReversed() {
+        return false; 
+    }
+	// WAYPOINT_DATA: [{"position":{"x":280,"y":20},"speed":60,"radius":0,"comment":""},{"position":{"x":250,"y":20},"speed":60,"radius":20,"comment":""},{"position":{"x":105,"y":100},"speed":60,"radius":40,"comment":""},{"position":{"x":60,"y":65},"speed":60,"radius":0,"comment":""}]
+	// IS_REVERSED: false
+	// FILE_NAME: SixBallPath4
+}
