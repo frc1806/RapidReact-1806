f5af7239da14ce91851705874aabdb9670fbd5d3
swat1806 <frc1806@gmail.com>
2022-03-05 20:33:47 -0600

    fixed auto modes

--- RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/auto/modes/TwoBallTopMode.java
+++ RapidReact-1806/src/main/java/org/usfirst/frc/team1806/robot/auto/modes/TwoBallTopMode.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1806.robot.auto.modes;
 import org.usfirst.frc.team1806.robot.auto.actions.SuperstructureActions.IntakeFront;
 import org.usfirst.frc.team1806.robot.auto.actions.SuperstructureActions.Shoot;
 import org.usfirst.frc.team1806.robot.auto.actions.SuperstructureActions.StopIntake;
+import org.usfirst.frc.team1806.robot.auto.actions.actionUtil.DrivePathAction;
 import org.usfirst.frc.team1806.robot.auto.actions.actionUtil.ResetPoseFromPathAction;
 import org.usfirst.frc.team1806.robot.auto.modes.modesUtil.AutoModeBase;
 import org.usfirst.frc.team1806.robot.auto.modes.modesUtil.AutoModeEndedException;
@@ -17,9 +18,10 @@ public class TwoBallTopMode extends AutoModeBase {
 
         runAction(new IntakeFront());
         runAction(new ResetPoseFromPathAction(new SixBallPath1()));
+        runAction(new DrivePathAction(new SixBallPath1()));
         runAction(new StopIntake());
         runAction(new Shoot(new Shot(175.0, 2500.0, 2300.0, false, false), 0.5));
-        runAction(new ResetPoseFromPathAction(new TwoBallTopBackMore()));
+        runAction(new DrivePathAction(new TwoBallTopBackMore()));
     }
     
 }
